9eb1d198aa6ef30a8b68cd68cc681a42534b1837
Vince Richter <r2d2fan20@outlook.com>
2020-04-28 15:02:28 -0500

    Update

--- index.js
+++ index.js
@@ -5,14 +5,31 @@ fs.exists("input.txt", (exists, err) => {
     if (exists) {
         fs.readFile("input.txt", "utf-8", (err, data) => {
             if (err) throw err;
-            var names = data.split("\n");
+            var names = data.split("\n"),
+                shortest = 0,
+                longest = 0,
+                longestName = "",
+                shortestName = "";
             for (var i = 0; i < names.length; i++) {
                 var name = names[i];
                 var nameLength = name.split("").length;
                 console.log("----------");
                 console.log("Name: " + name);
                 console.log("Length: " + nameLength);
+                if (i == 0 || nameLength < shortest) {
+                    //First time looping or shorter than the last one
+                    shortest = nameLength;
+                    shortestName = name;
+                }
+
+                if (nameLength > longest) {
+                    longest = nameLength;
+                    longestName = name;
+                }
             }
+            console.log("----------------------");
+            console.log(`Longest Name: ${longestName} (${longest})`);
+            console.log(`Shortest Name: ${shortestName} (${shortest})`);
         })
     }
 });
